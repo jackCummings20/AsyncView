c4ecb340c469cc481bda0e67a39a20cc48af2e19
Jack Cummings <jackcummings14@icloud.com>
2022-07-19 11:41:01 -0400

    Add public init to AsyncResultView

--- Sources/AsyncView/AsyncResultView.swift
+++ Sources/AsyncView/AsyncResultView.swift
@@ -7,13 +7,20 @@
 
 import SwiftUI
 
-public struct AsyncResultView<Source: AsyncLoadableObject, EmptyView: View, ErrorView: View, ContentView: View>: View {
+public struct AsyncResultView<Source: AsyncLoadableObject, EmptyView: View, ErrorView: View, ContentView: View>: View {    
     
     @ObservedObject public var source: Source
     
     public var emptyView: () -> EmptyView
     public var errorView: (Error) -> ErrorView
     public var contentView: (Source.Output) -> ContentView
+    
+    public init(source: Source, emptyView: @escaping () -> EmptyView, errorView: @escaping (Error) -> ErrorView, contentView: @escaping (Source.Output) -> ContentView) {
+        self.source = source
+        self.emptyView = emptyView
+        self.errorView = errorView
+        self.contentView = contentView
+    }
     
     public var body: some View {
         switch source.state {
